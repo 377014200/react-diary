2d41bdded2676ade4c3d063a1916d5a7f0213af9
DBL <377014200@qq.com>
2019-09-10 14:30:24 +0800

    添加 ref 菜单

--- src/view/Sidebar/Sidebar.jsx
+++ src/view/Sidebar/Sidebar.jsx
@@ -24,6 +24,7 @@ export default class Sidebar extends Component {
                   <li><NavLink to='/context' activeClassName='route-active' > Context </NavLink></li>
                   <li><NavLink to='/fragments' activeClassName='route-active' > Fragments </NavLink></li>
                   <li><NavLink to='/mixins' activeClassName='route-active' > mixins </NavLink></li>
+                  <li><NavLink to='/ref' activeClassName='route-active' > ref </NavLink></li>
 
                </ul>
          </div>
