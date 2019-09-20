dcfe160d597a1f2ba7694cec6c6bc3d0b2c980f6
DBL <377014200@qq.com>
2019-09-20 09:40:09 +0800

    添加 blockquote 公用组件

---
+++ src/components/Blockquote.jsx
@@ -0,0 +1,33 @@
+import { PureComponent, Children } from 'react';
+import './blockquote-style.scoped.css';
+
+export default class Blockquote extends PureComponent {
+
+   render(){
+      const childrens = Children.toArray( this.props.children );
+      return (
+         <blockquote>
+            { childrens }
+         </blockquote>
+      )
+   }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
