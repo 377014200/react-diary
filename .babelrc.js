b027c886605ddcaee78009b7f38838d50c3633a0
DBL <377014200@qq.com>
2019-09-20 08:52:56 +0800

    添加对es7修饰器的支持

--- .babelrc.js
+++ .babelrc.js
@@ -1,12 +1,18 @@
 module.exports = {
    "presets": ["@babel/preset-react"],
    "plugins": [
+      "babel-plugin-react-scoped-css",
+      "@babel/plugin-syntax-dynamic-import",
+      [
+         "@babel/plugin-proposal-decorators", {
+            "legacy": true
+         }
+      ],
       [
          "@babel/plugin-proposal-class-properties", {
             "loose": true
          }
       ],
-      "babel-plugin-react-scoped-css",
-      "@babel/plugin-syntax-dynamic-import",
+
    ]
 }
