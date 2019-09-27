662807705ae255bee8021afda28eccbf775c8c70
DBL <377014200@qq.com>
2019-09-27 09:52:13 +0800

    取消使用 shouldRouterUpdate 高妎組建

--- src/view/Lifecycle hook functions/index.jsx
+++ src/view/Lifecycle hook functions/index.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
 
-import Mount from './Mount'
-import Updated from './Updated'
-import Fieldset from 'components/Fieldset'
-import shouldRouterUpdate from "HOC/shouldRouterUpdate";
+import Mount from './Mount';
+import Updated from './Updated';
+import Fieldset from 'components/Fieldset';
 
 {/*  生命周期钩子函数 */}
-@shouldRouterUpdate( '/hook' )
-export default  class Hook extends React.Component{
-   render(){
+export default class Hook extends React.Component {
+
+   render() {
 
       return (
-         <Fieldset title={ (<h2>组件的生命周期 <i>in Lifecycle hook function </i></h2>) }>
+         <Fieldset title={ ( <h2>组件的生命周期 <i>in Lifecycle hook function </i></h2> ) }>
             <strong>常用的生命周期方法会被加粗。其余生命周期函数的使用则相对罕见。</strong>
             <br/>
-            <a href="https://react.docschina.org/docs/react-component.html" target={'_blank'}>查看官方的更详细的文档</a>
+            <a href='https://react.docschina.org/docs/react-component.html' target={'_blank'}>查看官方的更详细的文档</a>
             <p>挂载</p>
             <Mount/>
             <p>更新</p>
@@ -32,7 +31,8 @@ export default  class Hook extends React.Component{
                </ul>
             </Fieldset>
          </Fieldset>
-      )
+      );
 
    }
+
 }
