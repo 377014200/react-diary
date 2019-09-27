54a420ec6ad443e5a2f3d572af7e8d3800d57964
DBL <377014200@qq.com>
2019-09-27 09:50:32 +0800

    添加 learn 組建

---
+++ src/view/Learn to stand/index.jsx
@@ -0,0 +1,10 @@
+export default function () {
+
+   return (
+      <div>
+         <p>有关 React，Redux，Javascript 和相关主题的其他大量教程，文章和其他资源，可以查看</p>
+         <a href='https://github.com/markerikson/react-redux-links' target='_blank'>React/Redux Links list</a>
+      </div>
+   );
+
+}
