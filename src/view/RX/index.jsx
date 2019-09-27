5e9d421c15327f286324b569203a274247bcc445
DBL <377014200@qq.com>
2019-09-27 09:57:28 +0800

    RX

---
+++ src/view/RX/index.jsx
@@ -0,0 +1,11 @@
+export default function () {
+
+   return (
+      <div>
+         <p>如果有缘分的化会再见面的</p>
+         <p>RxJS是一个库，用于使用可观察序列组合异步和基于事件的程序。它提供了一个核心类型，即Observable、附属类型(观察者、调度程序、主题)和受Array#extras (map、filter、reduce、every等)启发的操作符，以允许将异步事件作为集合来处理。</p>
+         <a href="https://rxjs.dev/" target="_blank">RX.js 官网</a>
+      </div>
+   )
+
+}
