8813caabb2f45eff3b53b3d8445ec7adc8afc911
DBL <377014200@qq.com>
2019-09-27 09:39:21 +0800

    .

--- src/components/Fieldset.jsx
+++ src/components/Fieldset.jsx
@@ -1,11 +1,10 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react';
 
 export default class Fieldset extends Component {
 
    static propTypes = {
-      children: PropTypes.node
+      children: Propypes.node
    };
-
 
    render() {
 
@@ -15,13 +14,13 @@ export default class Fieldset extends Component {
       };
 
       return (
-         <div style={ { margin:'10px 0', width: '100%'} }>
+         <div style={ { margin: '10px 0', width: '100%' } }>
             <fieldset>
                <legend style={legendStyle ? legendStyle : _legendStyle }> { title } </legend>
                {children}
             </fieldset>
          </div>
-      )
+      );
 
    }
 
