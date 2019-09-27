f4f79b3754f605fe7e3022ae6ae65d4d68a17ae9
DBL <377014200@qq.com>
2019-09-27 09:39:52 +0800

    添加 actions 入口文件

---
+++ src/redux/actions/index.js
@@ -0,0 +1,7 @@
+import * as test from './test';
+
+// 这样做是为了又没没有重名的功能函数
+export const toggleTodo = test.toggleTodo;
+export const addTodo = test.addTodo;
+export const setVisibilityFilter = test.setVisibilityFilter;
+
