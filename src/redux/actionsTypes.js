2456fe94c009037aac0362391112ea8e52efc476
DBL <377014200@qq.com>
2019-09-27 09:41:25 +0800

    actions-type

---
+++ src/redux/actionsTypes.js
@@ -0,0 +1,7 @@
+/* 你应该为它的用意是属于哪个模块做好标注这是为了更直观 */
+
+// of test reducer
+export const ADD_TODO = 'ADD_TODO';
+export const TOGGLE_TODO = 'TOGGLE_TODO';
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
+
