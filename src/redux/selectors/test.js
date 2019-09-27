cc26ade9cc5ea27a41ccf5848c2d281476018dce
DBL <377014200@qq.com>
2019-09-27 09:40:54 +0800

    添加 selectors test 模板

---
+++ src/redux/selectors/test.js
@@ -0,0 +1,44 @@
+import * as AT from '../actionsTypes';
+import warehouse, { selectorsTypes as ST } from '../state';
+
+export default {
+   [ST.visibilityFilter]( state = warehouse .visibilityFilter, action ) {
+
+      const visibilityArray = ['SHOW_ALL', 'SHOW_COMPLETED', 'SHOW_ACTIVE'];
+      const visibilityItem = visibilityArray.includes( action.filter ) ? action.filter : state;
+
+      return {
+         [AT.SET_VISIBILITY_FILTER]: visibilityItem
+      } [ action.type ] || state;
+
+   },
+   [ST.todos]( state = warehouse.todos, action ) {
+
+      return {
+         [AT.ADD_TODO]: [
+            ...state,
+            {
+               text: action.text,
+               completed: false
+            }
+         ],
+         [AT.TOGGLE_TODO]: state.map( ( todo, index ) => {
+
+            if ( index === action.index ) {
+
+               return Object.assign( {}, todo, {
+                  completed: !todo.completed
+               } );
+
+            }
+            return todo;
+
+         } )
+
+      } [ action.type ] || state;
+
+   }
+};
+
+
+
