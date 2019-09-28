e2136007bcf78ebbeab760828f059ea6243b3bce
DBL <377014200@qq.com>
2019-09-29 02:02:02 +0800

    .

--- src/view/react-redux and redex/case/Connect.jsx
+++ src/view/react-redux and redex/case/Connect.jsx
@@ -7,7 +7,6 @@ export class MapDispatchToProps extends React.PureComponent {
 
    render() {
 
-      console.log( this );
       return (
          <button onClick={ ()=> this.props.addTodoAsync( 123 ) }> 测试 connect 的第二参数</button>
       );
