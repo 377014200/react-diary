fd963a27839553ebf75936f155ec731cc98d9a32
DBL <377014200@qq.com>
2019-09-28 13:23:19 +0800

    测试 redux 异步处理程序

--- src/view/react-redux and redex/case/Connect.jsx
+++ src/view/react-redux and redex/case/Connect.jsx
@@ -1,15 +1,15 @@
 import { connect } from 'react-redux';
-import { addTodo } from '@/redux/actions/index';
+import { addTodoAsync } from '@/redux/actions';
 import mapStateToProps from '@/assets/javascript/mapStateToProps';
 
-@connect( mapStateToProps( 'todos', 'visibilityFilter' ), { addTodo } )
+@connect( mapStateToProps( 'todos', 'visibilityFilter' ), { addTodoAsync } )
 export class MapDispatchToProps extends React.PureComponent {
 
    render() {
 
       console.log( this );
       return (
-         <button onClick={ ()=> this.props.addTodo( 123 ) }> 测试 connect 的第二参数</button>
+         <button onClick={ ()=> this.props.addTodoAsync( 123 ) }> 测试 connect 的第二参数</button>
       );
 
    }
