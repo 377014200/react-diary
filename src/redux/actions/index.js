ae38d25d69d97ec9879e8953e2a85e1663cd7c2b
DBL <377014200@qq.com>
2019-09-28 13:24:47 +0800

    导出异步处理程序

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -3,5 +3,6 @@ import * as test from './test';
 // 这样做是为了没有重名的功能函数
 export const toggleTodo = test.toggleTodo;
 export const addTodo = test.addTodo;
+export const addTodoAsync = test.addTodoAsync;
 export const setVisibilityFilter = test.setVisibilityFilter;
 
