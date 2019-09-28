1e868b5351a866170eb68a64f745de583168a51b
DBL <377014200@qq.com>
2019-09-29 02:50:11 +0800

    添加全局 热更新

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
 import ReactDOM from 'react-dom';
-import App from './view/App.jsx';
 import { Provider } from 'react-redux';
 import store from '@/redux';
+import { AppContainer } from 'react-hot-loader';
 
-const app = (
-   <Provider store={store}>
-      <App />
-   </Provider>
-);
+import App from './view/App.jsx';
+const render = function ( Component ) {
 
-ReactDOM.render( app, document.getElementById( 'root' ) );
+   const RootApp = (
+      <AppContainer>
+         <Provider store={store}>
+            <Component />
+         </Provider>
+      </AppContainer>
+   );
 
+   ReactDOM.render( RootApp, document.getElementById( 'root' ) );
+
+};
+
+render( App );
+
+if ( module.hot ) {
+
+   module.hot.accept( './view/App.jsx', () => {
+
+      console.log( 'Accepting the updated in react!' );
+      render( App );
+
+   } );
+
+}
