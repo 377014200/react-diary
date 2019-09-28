c35962b5a56ad35f95b41994520bc25582b94017
DBL <377014200@qq.com>
2019-09-29 02:50:41 +0800

    2019/09/29

--- src/view/rhlConfig.js
+++ src/view/rhlConfig.js
@@ -2,7 +2,7 @@ import { setConfig } from 'react-hot-loader';
 // react-hot-loader 配置
 // 参考: https://github.com/gaearon/react-hot-loader/blob/master/index.d.ts#L62-L133
 setConfig( {
-   logLevel: 'log',
+   logLevel: 'error',
    // 显示“hot-loader / react-dom”警告
-   showReactDomPatchNotification: false
+   showReactDomPatchNotification: false,
 } );
