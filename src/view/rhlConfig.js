09b8f318f5b5d577df29045170695d99ae93e6d1
DBL <377014200@qq.com>
2019-09-29 02:00:57 +0800

    添加 react-hot-loader 配置

---
+++ src/view/rhlConfig.js
@@ -0,0 +1,8 @@
+import { setConfig } from 'react-hot-loader';
+// react-hot-loader 配置
+// 参考: https://github.com/gaearon/react-hot-loader/blob/master/index.d.ts#L62-L133
+setConfig( {
+   logLevel: 'log',
+   // 显示“hot-loader / react-dom”警告
+   showReactDomPatchNotification: false
+} );
