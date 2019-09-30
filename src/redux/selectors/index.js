38e4374d52650d001bb09d97657ceb895be31ff4
DBL <377014200@qq.com>
2019-09-30 21:00:00 +0800

    重写 selectors 让代码看起来更直观

--- src/redux/selectors/index.js
+++ src/redux/selectors/index.js
@@ -1,5 +1,18 @@
 import test from './test';
+import { selectorsTypes as ST } from '@/redux/state';
 
+function reducer( actionsManifests, placeholder ) {
+
+   return function ( state = placeholder, action ) {
+
+      const commit = actionsManifests[action.type ];
+
+      return commit ? commit( { state, action } ) : state;
+
+   };
+
+}
 export default {
-   ...test
+   [ST.visibilityFilter]: reducer( test, 'SHOW_ALL' ),
+   [ST.todos]: reducer( test, [] )
 };
