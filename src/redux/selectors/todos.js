f9ecff54f39611b7d02695867bc260c6fcd6e3e5
DBL <377014200@qq.com>
2019-10-07 21:33:07 +0800

    2019/10/07

--- src/redux/selectors/todos.js
+++ src/redux/selectors/todos.js
@@ -1,17 +1,13 @@
 import * as AT from '../actionsTypes';
 
-// 如何你理解 不可变 数据,  你就可以驾驭它;
-// state 是不应该改变的只能拷贝然后返回一个新的 state
+// 最好遵循 不可变 概念;
 export default {
    [AT.ADD_TODO]( { state, action } ) {
 
-      return [
-         ...state,
-         {
-            text: action.text,
-            completed: false
-         }
-      ];
+      return state.concat( {
+         text: action.text,
+         completed: false
+      } );
 
    },
    [AT.TOGGLE_TODO]( { state, action } ) {
@@ -20,9 +16,10 @@ export default {
 
          if ( index === action.index ) {
 
-            return Object.assign( {}, todo, {
+            return {
+               ...todo,
                completed: !todo.completed
-            } );
+            };
 
          }
          return todo;
