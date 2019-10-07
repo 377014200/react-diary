7ccd49683d23c2f99e35ccdbfadf08a81bdccddb
DBL <377014200@qq.com>
2019-10-07 21:32:19 +0800

    2019/10/07

--- src/view/mixins/mixins.jsx
+++ src/view/mixins/mixins.jsx
@@ -1,9 +1,8 @@
-import React from "react"
-import Code from "@/components/Code";
-import Fieldset from "@/components/Fieldset";
-import {codeCard} from './codeCard'
+import Code from '@/components/Code';
+import Fieldset from '@/components/Fieldset';
+import { codeCard } from './codeCard';
 
-export default function mixins(props) {
+export default function mixins() {
 
    return (
       <Fieldset
@@ -15,7 +14,7 @@ export default function mixins(props) {
          </p>
          <p>如果完全不同的组件有相似的功能，这就会产生“横切关注点（cross-cutting concerns）“问题。针对这个问题，在使用 createReactClass 创建 React 组件的时候，引入 mixins
             功能会是一个很好的解决方案。</p>
-         <a href="https://react.docschina.org/docs/react-without-es6.html" target='_blank'> 关于如何不使用 es6 的方式</a>
+         <a href='https://react.docschina.org/docs/react-without-es6.html' target='_blank'> 关于如何不使用 es6 的方式</a>
          <Fieldset
             title={<h3>使用 mixins 的方法和案例</h3>}
          >
@@ -24,5 +23,6 @@ export default function mixins(props) {
       </Fieldset>
 
 
-   )
+   );
+
 }
