5d6fef4b7ef7be3637d95f8fc07a190d70d9ecf4
DBL <377014200@qq.com>
2019-10-07 22:31:05 +0800

    2019/10/07

--- src/assets/javascript/mapStateToProps.js
+++ src/assets/javascript/mapStateToProps.js
@@ -1,21 +1,27 @@
-export default function mapStateToProps ( ...arg ) {
+export default function mapStateToProps ( coat ) {
 
    return function ( state ) {
 
       const props = {};
 
-      arg.forEach( function ( key ) {
+      if ( typeof coat === 'string' ) {
+
+         props[coat] = state[coat];
+         return props;
+
+      }
+      coat.forEach( function ( key ) {
 
          if ( typeof key !== 'string' ) {
 
-            console.log( '参数必须是字符串在 maoStateToProps 中' );
+            console.error( '参数必须是字符串在 maoStateToProps 中' );
             return;
 
          }
          props[key] = state[key];
 
       } );
-      return arg.length === 0 ? null : props;
+      return coat.length === 0 ? null : props;
 
    };
 
