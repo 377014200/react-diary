1c54297fa49e93e3f71a156d4774e21a2b2c214b
DBL <377014200@qq.com>
2019-10-07 22:30:45 +0800

    2019/10/07

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,8 +1,8 @@
-import * as test from './test';
+import * as creators from './actionCreators';
 
-// 这样做是为了没有重名的功能函数
-export const toggleTodo = test.toggleTodo;
-export const addTodo = test.addTodo;
-export const addTodoAsync = test.addTodoAsync;
-export const setVisibilityFilter = test.setVisibilityFilter;
+// 这样做是为了没有重名的功能函数而且你不会丢失它;
+export const toggleTodo = creators.toggleTodo;
+export const addTodo = creators.addTodo;
+export const addTodoAsync = creators.addTodoAsync;
+export const setVisibilityFilter = creators.setVisibilityFilter;
 
